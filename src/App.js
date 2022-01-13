cc1b68a141cbe6f46528daffcd09f8671d2dfdae
Sara Alexie Noomi Nederskov <6583335@rts-365.dk>
2022-01-13 09:38:07 +0100

    added goelocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import OneSignal from 'react-onesignal';
+import Location from './components/location';
 
 function App() {
 
@@ -13,20 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Location />
     </div>
   );
 }
